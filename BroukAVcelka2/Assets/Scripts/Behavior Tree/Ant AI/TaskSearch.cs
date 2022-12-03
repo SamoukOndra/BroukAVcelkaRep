de9c25021b18e117a8cc8252fce785516c3786c7
SamoukOndra <platno@seznam.cz>
2022-12-03 13:38:18 +1100

    AI skelet

---
+++ BroukAVcelka2/Assets/Scripts/Behavior Tree/Ant AI/TaskSearch.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorTree;
+
+public class TaskSearch : Node
+{
+    public TaskSearch()
+    {
+
+    }
+
+    /*public override NodeState Evaluate()
+    {
+        
+    }*/
+}
---
+++ BroukAVcelka2/Assets/Scripts/Notes/Note_Ants.cs
@@ -0,0 +1,15 @@
+/*Tasks for Ants:
+ * SEARCH
+ * AWARE OF PLAYER
+ * ATTACK PLAYER
+ * TRACKING
+ * HARVESTING
+ * ??STORAGING
+
+
+
+
+
+
+
+*/    
